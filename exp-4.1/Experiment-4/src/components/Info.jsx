55cf03db41b80cf6685932e414d344416b4fde0a
User <user@example.com>
2026-06-23 09:27:24 +0530

    Add exp-4.1 - src/components/Info.jsx file

---
+++ exp-4.1/Experiment-4/src/components/Info.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Info() {
+  return (
+    <div>
+        <h1 className="container">Student Information</h1>
+        <div className="card">
+            <h3>Rudra Pratap Yadav</h3>
+            <p>Course: Computer Science</p>
+            <p>Marks: 98</p>
+        </div>
+        <div className='card'> 
+            
+        
+            <h3>Rohit Kumar Verma</h3>
+            <p>Course: Information Technology</p>
+            <p>Marks: 92</p>
+
+        </div>
+        <div className='card'>
+            <h3>Robin Varshney</h3>
+            <p>Course: Electronics</p>
+            <p>Marks: 78</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Info
